03566c0fb0462f8d859dfe80203b1e80e27473c4
teaht <142947681+teaht@users.noreply.github.com>
2025-11-03 13:44:08 -0800

    Add files via upload
    Decision Tree with outcomes

---
+++ Decision_Tree/OBP_des_tree.R
@@ -0,0 +1,396 @@
+library(readxl)
+library(dplyr)
+library(MASS)
+library(car)
+library(nnet)
+library(glmnet)
+library(mgcv)
+library(arm)
+library(pROC)
+# des tree
+library(rpart)
+library(rpart.plot)
+
+
+
+#####################################################################
+#                     Data Load and Clean
+#####################################################################
+
+df <- read_excel("/Users/teahthies/Desktop/Modeling/cleanpitch.xlsx", sheet = 1)
+nrow(df) #6952
+colnames(df) # 27 columns
+
+#--------------------------------------------------------------------
+# Convert Columns
+#--------------------------------------------------------------------
+# Convert to numeric
+df$Inning <- as.numeric(df$Inning)
+# Convert to factors (categorical)
+df$Pitch <- as.factor(df$Pitch)
+df$Bat_hand <- as.factor(df$Bat_hand)
+df$code <- as.factor(df$code)
+df$Pitch_Result <- as.factor(df$Pitch_Result)
+df$home_vis <- as.factor(df$home_vis)
+df$hit_type <- as.factor(df$hit_type)
+df$Strike <- as.factor(df$Strike)
+df$Ball <- as.factor(df$Ball)
+df$pitch_count <- as.factor(df$pitch_count)
+# Convert to logical (binary)
+df$swing <- as.logical(df$swing)
+df$outcome <- as.factor(df$outcome)
+
+df <- df %>%
+  mutate(
+    Pitch = if_else(Pitch == "B", "C", Pitch),
+    Pitch = if_else(Pitch == "4", "F", Pitch),
+    Location = if_else(Location == "1f", "1", Location),
+    num_outs = if_else(num_outs == 4, 2, num_outs),
+    num_outs = if_else(num_outs == 3, 2, num_outs),
+    num_baserunners = if_else(num_baserunners == -1, 0, num_baserunners),
+    num_baserunners = if_else(num_baserunners == 5, 3, num_baserunners),
+    num_baserunners = if_else(num_baserunners == 4, 3, num_baserunners),
+    Pitcher = if_else(Pitcher == "G.", "Gonzales", Pitcher),
+    Pitcher = if_else(Pitcher == "Frutos", "Frutoz", Pitcher),
+    Strike = if_else(Strike == "2", "1", Strike)
+  )
+
+df$Location <- as.factor(df$Location)
+df$Strike <- as.logical(df$Strike)
+df$Ball <- as.logical(df$Ball)
+df$pitch_count <- as.numeric(df$pitch_count)
+
+
+
+#####################################################################
+#               Check Balance and Split Test and Train
+#####################################################################
+
+#--------------------------------------------------------------------
+# Question 1: OBP vs what pitch count and location?
+#--------------------------------------------------------------------
+# Prepare Data
+df <- subset(df, bat_event_fl == TRUE) # only get bat_event_fl
+nrow(df)
+# make a sub_fl column (this is walk flag column)
+df2 <- df %>%
+  mutate(sub_fl = ifelse(code %in% c(18, 20, 21, 22, 23), 1, 0))
+unique(df2$sub_fl)
+nrow(df2) #4039
+df2$sub_fl <- as.logical(df2$sub_fl)
+# .......................................................................
+### new split 
+library(dplyr)
+
+set.seed(42)  # for reproducibility
+
+#----------------------------
+# 1) Split into Train/Test
+#----------------------------
+########## conference split
+# train_df_raw <- df2 %>%
+#   filter(
+#     year %in% c(2023, 2024) |
+#       (year == 2025 & game_type == "non-conference")
+#   )
+# 
+# test_df <- df2 %>%
+#   filter(year == 2025 & game_type == "conference")
+
+########## last 20% split
+# total number of rows
+n <- nrow(df2)
+
+# index cutoff for 80%
+cutoff <- floor(0.80 * n)
+
+# split: first 80% = train, last 20% = test
+train_df_raw <- df2[1:cutoff, ]
+test_df      <- df2[(cutoff + 1):n, ]
+
+# sanity check
+nrow(train_df_raw)   # should be ~80% of total
+nrow(test_df)        # should be ~20% of total
+
+
+# (optional) check class balance pre-balance
+table(train_df_raw$sub_fl)
+prop.table(table(train_df_raw$sub_fl))
+table(test_df$sub_fl)
+prop.table(table(test_df$sub_fl))
+
+#-----------------------------------------
+# 2) Balance ONLY the training data
+#    (random oversampling of minority)
+#-----------------------------------------
+df_0 <- train_df_raw %>% filter(sub_fl == 0)
+df_1 <- train_df_raw %>% filter(sub_fl == 1)
+
+if (nrow(df_0) >= nrow(df_1)) {
+  df_1_os <- df_1 %>% sample_n(nrow(df_0), replace = TRUE)
+  train_df <- bind_rows(df_0, df_1_os)
+} else {
+  df_0_os <- df_0 %>% sample_n(nrow(df_1), replace = TRUE)
+  train_df <- bind_rows(df_0_os, df_1)
+}
+
+# shuffle rows (optional, but nice before modeling)
+train_df <- train_df %>% slice_sample(prop = 1)
+
+# sanity checks
+nrow(train_df)
+prop.table(table(train_df$sub_fl))
+# .......................................................................
+
+# convert to 0,1 instead of true and false
+train_df$sub_fl <- factor(as.integer(train_df$sub_fl), levels = c(0,1))
+test_df$sub_fl <- factor(as.integer(test_df$sub_fl), levels = c(0,1))
+
+#-----------------------------------------------------------------
+# Split RHH and LHH for reduced models 
+#-----------------------------------------------------------------
+# RHH train
+df_right_sub_train <- train_df %>% filter(Bat_hand == "R")
+df_right_sub_train <- subset(df_right_sub_train, select = -Bat_hand)
+# LHH train
+df_left_sub_train  <- train_df %>% filter(Bat_hand == "L")
+df_left_sub_train <- subset(df_left_sub_train, select = -Bat_hand)
+# RHH test
+df_right_sub_test <- test_df %>% filter(Bat_hand == "R")
+df_right_sub_test <- subset(df_right_sub_test, select = -Bat_hand)
+# LHH test
+df_left_sub_test  <- test_df %>% filter(Bat_hand == "L")
+df_left_sub_test <- subset(df_left_sub_test, select = -Bat_hand)
+
+
+
+
+
+
+
+#--------------------------------------
+# RHH 
+#--------------------------------------
+# clean data for model 
+cols_used <- c("strike_count", "sub_fl", "Inning", "Pitch", "Location", "line_up_pos", 
+               "OPP_score", "num_baserunners", "ball_count", "p_throws", "LBSU_score",
+               "num_outs", "home_vis", "p_throws")
+sapply(df_right_sub_train[cols_used], function(x) sum(is.na(x)))
+cols_used <- c("strike_count", "sub_fl", "Inning", "Pitch", "Location", "line_up_pos", 
+               "OPP_score", "num_baserunners", "ball_count", "p_throws", "LBSU_score",
+               "num_outs", "home_vis", "p_throws")
+sapply(df_right_sub_test[cols_used], function(x) sum(is.na(x)))
+
+# get rid of NA's in data we will use 
+df_clean_train <- df_right_sub_train[ , cols_used] 
+df_clean_train <- na.omit(df_clean_train) 
+df_clean_test <- df_right_sub_test[ , cols_used]  
+df_clean_test <- na.omit(df_clean_test)
+
+
+#-----------------------------------------------------------------
+# RHH Models
+#-----------------------------------------------------------------
+base_model <- rpart(sub_fl ~ Pitch+Location+
+                      line_up_pos+
+                      LBSU_score+
+                      OPP_score+
+                      num_outs+
+                      num_baserunners+
+                      ball_count+
+                      strike_count+
+                      Inning+
+                      home_vis+
+                      p_throws,
+                    data = df_clean_train, 
+                    method = "class",
+                    control = rpart.control(cp = 0.02))
+
+# Plot tree
+rpart.plot(base_model, type = 3, extra = 104, fallen.leaves = TRUE)
+#title("Reduced Tree: 83% Accuracy") 
+
+#-----------------------------
+# Train model predictions
+#-----------------------------
+test_probs <- predict(base_model, newdata = df_clean_train, type = "prob")
+nrow(test_probs)
+nrow(df_clean_train)
+predicted_classes <- ifelse(test_probs[, "1"] >= 0.5, TRUE, FALSE)
+confusion <- table(Predicted = predicted_classes, Actual = df_clean_train$sub_fl)
+print(confusion)
+
+TN <- confusion[1, 1]
+FP <- confusion[1, 2]
+FN <- confusion[2, 1]
+TP <- confusion[2, 2]
+
+# Metrics
+accuracy <- (TP + TN) / (TP + TN + FP + FN)
+precision <- TP / (TP + FP)
+recall <- TP / (TP + FN)                     # also known as sensitivity
+f1_score <- 2 * precision * recall / (precision + recall)
+specificity <- TN / (TN + FP)
+
+# AUC
+roc_obj <- roc(df_clean_train$sub_fl, test_probs[, 1])
+plot(roc_obj)
+
+# show metrics
+round(accuracy, 2)     #0.64
+round(precision, 2)    # 0.49
+round(recall, 2)       # 0.69
+round(f1_score, 2)      # 0.57
+round(specificity, 2)   # 0.61
+round(auc(roc_obj), 2)    # 0.66
+
+#-----------------------------
+# test model predictions
+#-----------------------------
+test_probs <- predict(base_model, newdata = df_clean_test, type = "prob")
+nrow(test_probs)
+nrow(df_clean_test)
+predicted_classes <- ifelse(test_probs[, "1"] >= 0.5, TRUE, FALSE)
+confusion <- table(Predicted = predicted_classes, Actual = df_clean_test$sub_fl)
+print(confusion)
+
+TN <- confusion[1, 1]
+FP <- confusion[1, 2]
+FN <- confusion[2, 1]
+TP <- confusion[2, 2]
+
+# Metrics
+accuracy <- (TP + TN) / (TP + TN + FP + FN)
+precision <- TP / (TP + FP)
+recall <- TP / (TP + FN)                     # also known as sensitivity
+f1_score <- 2 * precision * recall / (precision + recall)
+specificity <- TN / (TN + FP)
+
+# AUC
+roc_obj <- roc(df_clean_test$sub_fl, test_probs[, 1])
+plot(roc_obj)
+
+# show metrics
+round(accuracy, 2)     #0.60
+round(precision, 2)    # 0.49
+round(recall, 2)       # 0.66
+round(f1_score, 2)      # 0.56
+round(specificity, 2)   # 0.56
+round(auc(roc_obj), 2)    # 0.61
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------------------------------
+# LHH
+#--------------------------------------
+# clean data for model 
+cols_used <- c("strike_count", "sub_fl", "Inning", "Pitch", "Location", "line_up_pos", 
+               "OPP_score", "num_baserunners", "ball_count", "p_throws", "LBSU_score",
+               "num_outs", "home_vis", "p_throws")
+sapply(df_left_sub_train[cols_used], function(x) sum(is.na(x)))
+sapply(df_left_sub_test[cols_used], function(x) sum(is.na(x)))
+
+# get rid of NA's in data we will use 
+df_clean_train <- df_left_sub_train[ , cols_used] 
+df_clean_train <- na.omit(df_clean_train) 
+df_clean_test <- df_left_sub_test[ , cols_used]  
+df_clean_test <- na.omit(df_clean_test)
+
+
+base_model <- rpart(sub_fl ~ Pitch+Location+
+                      line_up_pos+
+                      LBSU_score+
+                      OPP_score+
+                      num_outs+
+                      num_baserunners+
+                      ball_count+
+                      strike_count+
+                      Inning+
+                      home_vis+
+                      p_throws, 
+                    data = df_clean_train, method = "class",
+                    control = rpart.control(cp = 0.01))
+
+# Plot tree
+rpart.plot(base_model, type = 3, extra = 104, fallen.leaves = TRUE)
+title("Base Tree: 62% Accuracy") 
+
+#-----------------------------
+# Train model predictions
+#-----------------------------
+test_probs <- predict(base_model, newdata = df_clean_train, type = "prob")
+nrow(test_probs)
+nrow(df_clean_train)
+predicted_classes <- ifelse(test_probs[, "1"] >= 0.5, TRUE, FALSE)
+confusion <- table(Predicted = predicted_classes, Actual = df_clean_train$sub_fl)
+print(confusion)
+
+TN <- confusion[1, 1]
+FP <- confusion[1, 2]
+FN <- confusion[2, 1]
+TP <- confusion[2, 2]
+
+# Metrics
+accuracy <- (TP + TN) / (TP + TN + FP + FN)
+precision <- TP / (TP + FP)
+recall <- TP / (TP + FN)                     # also known as sensitivity
+f1_score <- 2 * precision * recall / (precision + recall)
+specificity <- TN / (TN + FP)
+
+# AUC
+roc_obj <- roc(df_clean_train$sub_fl, test_probs[, 1])
+plot(roc_obj)
+
+# show metrics
+round(accuracy, 2)     #0.67
+round(precision, 2)    # 0.62
+round(recall, 2)       # 0.70
+round(f1_score, 2)      # 0.66
+round(specificity, 2)   # 0.64
+round(auc(roc_obj), 2)    # 0.69
+
+#-----------------------------
+# test model predictions
+#-----------------------------
+test_probs <- predict(base_model, newdata = df_clean_test, type = "prob")
+nrow(test_probs)
+nrow(df_clean_test)
+predicted_classes <- ifelse(test_probs[, "1"] >= 0.5, TRUE, FALSE)
+confusion <- table(Predicted = predicted_classes, Actual = df_clean_test$sub_fl)
+print(confusion)
+
+TN <- confusion[1, 1]
+FP <- confusion[1, 2]
+FN <- confusion[2, 1]
+TP <- confusion[2, 2]
+
+# Metrics
+accuracy <- (TP + TN) / (TP + TN + FP + FN)
+precision <- TP / (TP + FP)
+recall <- TP / (TP + FN)                     # also known as sensitivity
+f1_score <- 2 * precision * recall / (precision + recall)
+specificity <- TN / (TN + FP)
+
+# AUC
+roc_obj <- roc(df_clean_test$sub_fl, test_probs[, 1])
+plot(roc_obj)
+
+# show metrics
+round(accuracy, 2)     #0.62
+round(precision, 2)    # 0.61
+round(recall, 2)       # 0.68
+round(f1_score, 2)      # 0.64
+round(specificity, 2)   # 0.57*
+round(auc(roc_obj), 2)    # 0.65 
---
+++ Decision_Tree/SO_NEW_des_tree.R
@@ -0,0 +1,367 @@
+library(readxl)
+library(dplyr)
+library(MASS)
+library(car)
+library(nnet)
+library(glmnet)
+library(mgcv)
+library(arm)
+library(pROC)
+# des tree
+library(rpart)
+library(rpart.plot)
+
+
+
+#####################################################################
+#                     Data Load and Clean
+#####################################################################
+
+df <- read_excel("/Users/teahthies/Desktop/Modeling/cleanpitch.xlsx", sheet = 1)
+nrow(df) #6952
+colnames(df) # 27 columns
+#--------------------------------------------------------------------
+# Convert Columns
+#--------------------------------------------------------------------
+# Convert to numeric
+df$Inning <- as.numeric(df$Inning)
+# Convert to factors (categorical)
+df$Pitch <- as.factor(df$Pitch)
+df$Bat_hand <- as.factor(df$Bat_hand)
+df$code <- as.factor(df$code)
+df$Pitch_Result <- as.factor(df$Pitch_Result)
+df$home_vis <- as.factor(df$home_vis)
+df$hit_type <- as.factor(df$hit_type)
+df$Strike <- as.factor(df$Strike)
+df$Ball <- as.factor(df$Ball)
+df$pitch_count <- as.factor(df$pitch_count)
+# Convert to logical (binary)
+df$swing <- as.logical(df$swing)
+df$outcome <- as.factor(df$outcome)
+
+df <- df %>%
+  mutate(
+    Pitch = if_else(Pitch == "B", "C", Pitch),
+    Pitch = if_else(Pitch == "4", "F", Pitch),
+    Location = if_else(Location == "1f", "1", Location),
+    num_outs = if_else(num_outs == 4, 2, num_outs),
+    num_outs = if_else(num_outs == 3, 2, num_outs),
+    num_baserunners = if_else(num_baserunners == -1, 0, num_baserunners),
+    num_baserunners = if_else(num_baserunners == 5, 3, num_baserunners),
+    num_baserunners = if_else(num_baserunners == 4, 3, num_baserunners),
+    Pitcher = if_else(Pitcher == "G.", "Gonzales", Pitcher),
+    Pitcher = if_else(Pitcher == "Frutos", "Frutoz", Pitcher),
+    Strike = if_else(Strike == "2", "1", Strike)
+  )
+
+df$Location <- as.factor(df$Location)
+df$Strike <- as.logical(df$Strike)
+df$Ball <- as.logical(df$Ball)
+df$pitch_count <- as.numeric(df$pitch_count)
+
+
+
+#####################################################################
+#               Check Balance and Split Test and Train
+#####################################################################
+
+#--------------------------------------------------------------------
+# Question 1: Strikeouts vs what pitch count and location?
+#--------------------------------------------------------------------
+# Prepare Data
+df <- subset(df, bat_event_fl == TRUE) # only get bat_event_fl
+nrow(df)
+# make a strikeout_fl column
+df <- df %>%
+  mutate(strikeout_fl = ifelse(code == 4, 1, 0))
+unique(df$strikeout_fl)
+df_strike2 <- df %>% filter(strike_count == 2)
+nrow(df_strike2)
+df_strike2$strikeout_fl <- as.logical(df_strike2$strikeout_fl)
+
+library(dplyr)
+
+set.seed(42)  # for reproducibility
+
+# total number of rows
+n <- nrow(df_strike2)
+
+# index cutoff for 80%
+cutoff <- floor(0.80 * n)
+
+# split: first 80% = train, last 20% = test
+train_df_raw <- df_strike2[1:cutoff, ]
+test_df      <- df_strike2[(cutoff + 1):n, ]
+
+# sanity check
+nrow(train_df_raw)   # should be ~80% of total
+nrow(test_df)        # should be ~20% of total
+
+# (optional) check class balance pre-balance
+table(train_df_raw$strikeout_fl)
+prop.table(table(train_df_raw$strikeout_fl))
+table(test_df$strikeout_fl)
+prop.table(table(test_df$strikeout_fl))
+
+#-----------------------------------------
+# 2) Balance ONLY the training data
+#    (random oversampling of minority)
+#-----------------------------------------
+df_0 <- train_df_raw %>% filter(strikeout_fl == 0)
+df_1 <- train_df_raw %>% filter(strikeout_fl == 1)
+
+if (nrow(df_0) >= nrow(df_1)) {
+  df_1_os <- df_1 %>% sample_n(nrow(df_0), replace = TRUE)
+  train_df <- bind_rows(df_0, df_1_os)
+} else {
+  df_0_os <- df_0 %>% sample_n(nrow(df_1), replace = TRUE)
+  train_df <- bind_rows(df_0_os, df_1)
+}
+
+# shuffle rows (optional, but nice before modeling)
+train_df <- train_df %>% slice_sample(prop = 1)
+
+# sanity checks
+nrow(train_df)
+prop.table(table(train_df$strikeout_fl))
+
+nrow(train_df)
+nrow(test_df)
+
+#-----------------------------------------------------------------
+# Split RHH and LHH for reduced models 
+#-----------------------------------------------------------------
+# RHH train
+df_right_sub_train <- train_df %>% filter(Bat_hand == "R")
+df_right_sub_train <- subset(df_right_sub_train, select = -Bat_hand)
+# LHH train
+df_left_sub_train  <- train_df %>% filter(Bat_hand == "L")
+df_left_sub_train <- subset(df_left_sub_train, select = -Bat_hand)
+# RHH test
+df_right_sub_test <- test_df %>% filter(Bat_hand == "R")
+df_right_sub_test <- subset(df_right_sub_test, select = -Bat_hand)
+# LHH test
+df_left_sub_test  <- test_df %>% filter(Bat_hand == "L")
+df_left_sub_test <- subset(df_left_sub_test, select = -Bat_hand)
+
+
+
+#-----------------------------------------------------------------
+# RHH Models
+#-----------------------------------------------------------------
+# clean data for model 
+cols_used <- c("strike_count", "strikeout_fl", "Inning", "Pitch", "Location", "line_up_pos", 
+               "OPP_score", "num_baserunners", "ball_count", "p_throws", "LBSU_score",
+               "num_outs", "home_vis")
+sapply(df_right_sub_train[cols_used], function(x) sum(is.na(x)))
+sapply(df_right_sub_test[cols_used], function(x) sum(is.na(x)))
+
+# get rid of NA's in data we will use 
+df_clean_train <- df_right_sub_train[ , cols_used] 
+df_clean_train <- na.omit(df_clean_train) 
+df_clean_test <- df_right_sub_test[ , cols_used]  
+df_clean_test <- na.omit(df_clean_test)
+
+nrow(df_clean_train)
+#-----------------------------------------------------------------
+# RHH Models
+#-----------------------------------------------------------------
+base_model <- rpart(strikeout_fl ~ Pitch+Location+
+                      line_up_pos+
+                      LBSU_score+
+                      OPP_score+
+                      num_outs+
+                      num_baserunners+
+                      ball_count+
+                      #strike_count+
+                      Inning+
+                      home_vis+
+                      p_throws,
+                    data = df_clean_train, 
+                    method = "class",
+                    control = rpart.control(cp = 0.02))
+
+# Plot tree
+rpart.plot(base_model, type = 3, extra = 104, fallen.leaves = TRUE)
+
+#-----------------------------
+# Train model predictions
+#-----------------------------
+test_probs <- predict(base_model, newdata = df_clean_train, type = "prob")
+nrow(test_probs)
+nrow(df_clean_train)
+unique(df_clean_train$strikeout_fl)
+predicted_classes <- ifelse(test_probs[, "TRUE"] >= 0.5, TRUE, FALSE)
+confusion <- table(Predicted = predicted_classes, Actual = df_clean_train$strikeout_fl)
+print(confusion)
+
+TN <- confusion[1, 1]
+FP <- confusion[1, 2]
+FN <- confusion[2, 1]
+TP <- confusion[2, 2]
+
+# Metrics
+accuracy <- (TP + TN) / (TP + TN + FP + FN)
+precision <- TP / (TP + FP)
+recall <- TP / (TP + FN)                     # also known as sensitivity
+f1_score <- 2 * precision * recall / (precision + recall)
+specificity <- TN / (TN + FP)
+
+# AUC
+roc_obj <- roc(df_clean_train$strikeout_fl, test_probs[, 1])
+plot(roc_obj)
+
+# show metrics
+round(accuracy, 2)     #0.67
+round(precision, 2)    # 0.81
+round(recall, 2)       # 0.66
+round(f1_score, 2)      # 0.73
+round(specificity, 2)   # 0.69
+round(auc(roc_obj), 2)    # 0.69
+
+#-----------------------------
+# test model predictions
+#-----------------------------
+test_probs <- predict(base_model, newdata = df_clean_test, type = "prob")
+nrow(test_probs)
+nrow(df_clean_test)
+predicted_classes <- ifelse(test_probs[, "TRUE"] >= 0.5, TRUE, FALSE)
+confusion <- table(Predicted = predicted_classes, Actual = df_clean_test$strikeout_fl)
+print(confusion)
+
+TN <- confusion[1, 1]
+FP <- confusion[1, 2]
+FN <- confusion[2, 1]
+TP <- confusion[2, 2]
+
+# Metrics
+accuracy <- (TP + TN) / (TP + TN + FP + FN)
+precision <- TP / (TP + FP)
+recall <- TP / (TP + FN)                     # also known as sensitivity
+f1_score <- 2 * precision * recall / (precision + recall)
+specificity <- TN / (TN + FP)
+
+# AUC
+roc_obj <- roc(df_clean_test$strikeout_fl, test_probs[, 1])
+plot(roc_obj)
+
+# show metrics
+round(accuracy, 2)     #0.68
+round(precision, 2)    # 0.60
+round(recall, 2)       # 0.73
+round(f1_score, 2)      # 0.66
+round(specificity, 2)   # 0.65
+round(auc(roc_obj), 2)    # 0.72
+
+
+
+
+
+#--------------------------------------
+# LHH Reduced Model - Location
+#--------------------------------------
+# clean data for model 
+cols_used <- c("strike_count", "strikeout_fl", "Inning", "Pitch", "Location", "line_up_pos", 
+               "OPP_score", "num_baserunners", "ball_count", "p_throws", "LBSU_score",
+               "num_outs", "home_vis", "p_throws")
+sapply(df_left_sub_train[cols_used], function(x) sum(is.na(x)))
+cols_used <- c("strike_count", "strikeout_fl", "Inning", "Pitch", "Location", "line_up_pos", 
+               "OPP_score", "num_baserunners", "ball_count", "p_throws", "LBSU_score",
+               "num_outs", "home_vis", "p_throws")
+sapply(df_left_sub_test[cols_used], function(x) sum(is.na(x)))
+
+# get rid of NA's in data we will use 
+df_clean_train <- df_left_sub_train[ , cols_used] 
+df_clean_train <- na.omit(df_clean_train) 
+df_clean_test <- df_left_sub_test[ , cols_used]  
+df_clean_test <- na.omit(df_clean_test)
+
+
+reduced_model <- rpart(strikeout_fl ~ Pitch+Location+
+                         line_up_pos+
+                         LBSU_score+
+                         OPP_score+
+                         num_outs+
+                         num_baserunners+
+                         ball_count+
+                         #strike_count+
+                         Inning+
+                         home_vis+
+                         p_throws, 
+                       data = df_clean_train, method = "class",
+                       control = rpart.control(cp = 0.03))
+
+# Plot tree
+rpart.plot(reduced_model, type = 3, extra = 104, fallen.leaves = TRUE)
+title("Base Tree: 65% Accuracy") 
+
+#-----------------------------
+# Train model predictions
+#-----------------------------
+test_probs <- predict(reduced_model, newdata = df_clean_train, type = "prob")
+nrow(test_probs)
+nrow(df_clean_train)
+predicted_classes <- ifelse(test_probs[, "TRUE"] >= 0.5, TRUE, FALSE)
+confusion <- table(Predicted = predicted_classes, Actual = df_clean_train$strikeout_fl)
+print(confusion)
+
+TN <- confusion[1, 1]
+FP <- confusion[1, 2]
+FN <- confusion[2, 1]
+TP <- confusion[2, 2]
+
+# Metrics
+accuracy <- (TP + TN) / (TP + TN + FP + FN)
+precision <- TP / (TP + FP)
+recall <- TP / (TP + FN)                     # also known as sensitivity
+f1_score <- 2 * precision * recall / (precision + recall)
+specificity <- TN / (TN + FP)
+
+# AUC
+roc_obj <- roc(df_clean_train$strikeout_fl, test_probs[, 1])
+plot(roc_obj)
+
+# show metrics
+round(accuracy, 2)     #0.72
+round(precision, 2)    # 0.59
+round(recall, 2)       # 0.74
+round(f1_score, 2)      # 0.65
+round(specificity, 2)   # 0.71
+round(auc(roc_obj), 2)    # 0.71
+
+#-----------------------------
+# test model predictions
+#-----------------------------
+test_probs <- predict(reduced_model, newdata = df_clean_test, type = "prob")
+nrow(test_probs)
+nrow(df_clean_test)
+predicted_classes <- ifelse(test_probs[, "TRUE"] >= 0.5, TRUE, FALSE)
+confusion <- table(Predicted = predicted_classes, Actual = df_clean_test$strikeout_fl)
+print(confusion)
+
+TN <- confusion[1, 1]
+FP <- confusion[1, 2]
+FN <- confusion[2, 1]
+TP <- confusion[2, 2]
+
+# Metrics
+accuracy <- (TP + TN) / (TP + TN + FP + FN)
+precision <- TP / (TP + FP)
+recall <- TP / (TP + FN)                     # also known as sensitivity
+f1_score <- 2 * precision * recall / (precision + recall)
+specificity <- TN / (TN + FP)
+
+# AUC
+roc_obj <- roc(df_clean_test$strikeout_fl, test_probs[, 1])
+plot(roc_obj)
+
+# show metrics
+round(accuracy, 2)     #0.52
+round(precision, 2)    # 0.31
+round(recall, 2)       # 0.44
+round(f1_score, 2)      # 0.36
+round(specificity, 2)   # 0.56
+round(auc(roc_obj), 2)    # 0.45
+
+
+
